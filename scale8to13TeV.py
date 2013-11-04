0449fc032c400b7401a280c3986c0025ed8b7fcb
Andrea <massironi.andrea@gmail.com>
2013-11-04 12:13:21 +0100

    update new scale factors

--- scale8to13TeV.py
+++ scale8to13TeV.py
@@ -1,20 +1,22 @@
+#
 # from http://ceballos.web.cern.ch/ceballos/mcfm/xsec_several_ecm.txt
 #
+# new: https://ceballos.web.cern.ch/ceballos/mcfm/xsec_several_ecm_MCFM66.txt
+#
 
-scaleFactor.update({'ggH'  :  2.97442/1.28090})
-scaleFactor.update({'qqH'  :  4.172/1.578    })  # @14 TeV
-scaleFactor.update({'WH'   :  1.504/0.7046   })  # @14 TeV
-scaleFactor.update({'ZH'   :  0.8830/0.4153  })  # @14 TeV
+scaleFactor.update({'ggH'  :  2.96933/1.28032    })  # @13 TeV
+scaleFactor.update({'qqH'  :  2.59943/1.24261    })  # @13 TeV
+scaleFactor.update({'WH'   :  2.27096/1.20447    })  # @13 TeV
+scaleFactor.update({'ZH'   :  2.44912/1.22665    })  # @13 TeV
 
 
-scaleFactor.update({'DYee' :  1.88476/1.14951})
-scaleFactor.update({'DYmm' :  1.88476/1.14951})
-scaleFactor.update({'DYTT' :  1.88476/1.14951})
+scaleFactor.update({'DYee' :  1.68986/1.12177})
+scaleFactor.update({'DYmm' :  1.68986/1.12177})
+scaleFactor.update({'DYTT' :  1.68986/1.12177})
 
-scaleFactor.update({'WJet' :  1.94063/1.15786*0.7})  # 0.7 is scale factor for 2012 ?
-#scaleFactor.update({'WJet' :  1.94063/1.15786})
+scaleFactor.update({'WJet' :  1.88638/1.15035})
 
-scaleFactor.update({'WW'   :  2.37867/1.21510})
-scaleFactor.update({'VV'   :  2.51853/1.23344})   #WZ
+scaleFactor.update({'WW'   :  2.85973/1.22092})
+scaleFactor.update({'VV'   :  2.30068/1.20664})   #WZ
 
-scaleFactor.update({'Top'  :  4.71947/1.42982})
+scaleFactor.update({'Top'  :  4.73131/1.43090})
