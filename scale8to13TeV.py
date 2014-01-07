117c659891adab5215f4b9e733162216ec3603dc
Andrea <massironi.andrea@gmail.com>
2014-01-07 11:09:35 +0100

    wwewk added and scale factors added

--- scale8to13TeV.py
+++ scale8to13TeV.py
@@ -10,13 +10,23 @@ scaleFactor.update({'WH'   :  2.27096/1.20447    })  # @13 TeV
 scaleFactor.update({'ZH'   :  2.44912/1.22665    })  # @13 TeV
 
 
-scaleFactor.update({'DYee' :  1.68986/1.12177})
-scaleFactor.update({'DYmm' :  1.68986/1.12177})
-scaleFactor.update({'DYTT' :  1.68986/1.12177})
+scaleFactor.update({'DYee'   :  1.68986/1.12177})
+scaleFactor.update({'DYmm'   :  1.68986/1.12177})
+scaleFactor.update({'DYee05' :  1.68986/1.12177}) # for WWewk
+scaleFactor.update({'DYmm05' :  1.68986/1.12177}) # for WWewk
+scaleFactor.update({'DYTT'   :  1.68986/1.12177})
 
 scaleFactor.update({'WJet' :  1.88638/1.15035})
 
 scaleFactor.update({'WW'   :  2.85973/1.22092})
 scaleFactor.update({'VV'   :  2.30068/1.20664})   #WZ
 
-scaleFactor.update({'Top'  :  4.73131/1.43090})
+scaleFactor.update({'WWewk'   :  2.85973/1.22092})
+
+scaleFactor.update({'Top'     :  4.73131/1.43090})
+scaleFactor.update({'TopPt0'  :  4.73131/1.43090}) # for WWewk
+scaleFactor.update({'TopPt1'  :  4.73131/1.43090}) # for WWewk
+
+
+scaleFactor.update({'Vg'   :  1.92736/1.16101})   #W^+ g
+
